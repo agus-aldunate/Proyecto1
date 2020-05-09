7847f0d6eb96c4ae9bb82dfa7214f9224fad559e
EmiSan <s.faraldo122@gmail.com>
2020-05-09 18:54:48 -0300

    cambio navbar a carpeta php

--- js/function.js
+++ js/function.js
@@ -1,13 +1,13 @@
   function hfindex(){
     $("#header").load("header.html"); 
     $("#footer").load("footer.html"); 
-    $("#navbar").load("navbar.php"); 
+    $("#navbar").load("php/navbar.php"); 
   };
 
   function hfabout(){
     $("#header").load("../header.html"); 
     $("#footer").load("../footer.html"); 
-    $("#navbar").load("../navbar.php"); 
+    $("#navbar").load("../php/navbar.php"); 
   };
 
 
